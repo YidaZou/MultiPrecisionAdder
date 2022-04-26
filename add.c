c4b1da5183ae92d8cf823d2e44dd180b6994f943
Yida Zou <66279535+YidaZou@users.noreply.github.com>
2022-04-26 12:50:46 -0500

    Add files via upload

---
+++ add.c
@@ -0,0 +1,25 @@
+int add (unsigned int A[], unsigned int B[], unsigned int C[]) {
+        unsigned int c;
+
+        c = A[0] + B[0];
+        C[0] = c;
+        c = A[1] + B[1];
+        C[1] = c;
+        c = A[2] + B[2];
+        C[2] = c;
+        c = A[3] + B[3];
+        C[3] = c;
+        c = A[4] + B[4];
+        C[4] = c;
+        c = A[5] + B[5];
+        C[5] = c;
+        c = A[6] + B[6];
+        C[6] = c;
+        c = A[7] + B[7];
+        C[7] = c;
+        c = A[8] + B[8];
+        C[8] = c;
+        c = A[9] + B[9];
+        C[9] = c;
+        return c;
+}
